8ce5602d2eb910bc08dcd43aea3babbc91db4079
Liangdi <wuliangdi@126.com>
2013-07-25 22:41:28 +0800

    完成document 和 其他元素的绑定

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-no-blinking-input
+无光标输入框
 =================
 
 Javascrip 实现无光标输入
+
+使用方法
+=================
+依赖于jQuery
+
+	<script type="text/javascript" src="jquery.min.js"></script>
+	<script type="text/javascript" src="no-blinking-input.js"></script>
+	<script type="text/javascript">
+			$(function(){
+				noBlinkingInput.newInstance({
+					context:$(".input"), //默认为document
+					debug:false
+				},function(e,val){
+					// e: keyup event
+					// val: input value
+					//console.log("here",e,val)
+					if(val == ""){
+						$(".output").html("&nbsp;")
+						
+					} else {
+						$(".output").html(val)
+						
+					}
+				})
+			})
+	</script>
+
+详细看 examples 目录文件
+
+* * *
+清除隐藏表单的内容并触发keyup事件
+
+	noBlinkingInput.cleanValue()
+其他
+=================
+未在IE平台测试
---
+++ no-blinking-input.js
@@ -0,0 +1,99 @@
+/*! 
+ *No Blinking Input JavaScript Library
+ *
+ *Copyright (c) 2013 Liangdi wu@liangdi.me 
+ *
+ *Released under the MIT license
+ */
+(function($) {
+	var inputId = "no-blinking-input";
+	var noBlinkingInput = window.noBlinkingInput = {
+		instances: {},
+		/**
+		 *  清除隐藏表单内容
+		 * @param {type} instanceId
+		 * @returns {undefined}
+		 */
+		cleanValue: function(instanceId) {
+			if (this.instances[instanceId]) {
+				$("#" + instanceId).val("").trigger("keyup")
+			} else {
+				for (var p in this.instances) {
+					$("#" + p).val("").trigger("keyup")
+				}
+			}
+		},
+		/**
+		 * 
+		 * @param {type} config
+		 *	config.context:绑定输入事件的上下文对象,默认为 document
+		 *	config.debug:调试模式
+		 * @param {type} callback	
+		 *	回调方法
+		 * @returns {undefined}
+		 */
+		newInstance: function(config, callback) {
+			var trueId = inputId + (Math.random() + "").substring(2);
+			var context = config.context || document;
+
+			var inputStr = '<textarea id="' + trueId + '"></textarea>';
+
+
+			var debug = config.debug || false;
+			if (!debug) {
+				$(inputStr).css("width", "0px").css("height", "0px")
+						.css("opacity", 0)
+						.css("position", "absolute")
+						.appendTo("body");
+			} else {
+				$("body").append($(inputStr).css("position", "absolute"));
+			}
+
+			$(context).on("mousemove", function(e) {
+
+				var w = $(context).width();
+				var h = $(context).height();
+				var offset = $(context).offset();
+				/**
+				 * 如果不是document 需要加上 offset 偏差
+				 */
+				if (offset) {
+					w += offset.left;
+					h += offset.top;
+				}
+				w = (w - 10) < e.clientX ? w - 10 : e.clientX;
+				h = (h - 10) < e.clientY ? h - 10 : e.clientY;
+
+				if (debug) {
+					console.log(w, h, e.clientX, e.clientY, e)
+					console.log($(context).offset())
+				}
+				/**
+				 * textarea 跟随 光标移动 这样 中文输入的时候 可以正常显示输入法位置
+				 */
+				$("#" + trueId).css("left", w).css("top", h);
+				setTimeout(function() {
+					$("#" + trueId).focus();
+				}, 100);
+			}).on("focus", function() {
+				setTimeout(function() {
+					$("#" + trueId).focus();
+				}, 100);
+			});
+			/**
+			 * 这里使用 keyup事件作为回调触发
+			 */
+			var inputMethod = 'keyup';
+			$("#" + trueId).on(inputMethod, function(e) {
+
+				var value = $(this).val();
+				if (typeof callback === "function") {
+					callback(e, value);
+				}
+
+			});
+			this.instances[trueId] = true;
+			return trueId;
+		}
+	};
+})(window.jQuery);
